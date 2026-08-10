a5e0b4d19305098f9174af1496488b46ad09a6b6
mjlovaisa-123 <cpnlovaisa@gmail.com>
2026-08-10 08:34:31 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import AuthGate from "./AuthGate.jsx";
 import { installStorageShim } from "./lib/storageShim.js";
 
 installStorageShim();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthGate />
   </React.StrictMode>
 );
